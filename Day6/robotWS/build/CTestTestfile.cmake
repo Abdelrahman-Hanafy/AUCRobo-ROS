330b78f9189fcfc63701d53bd6614514dd68ab7e
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2020-08-27 17:48:50 +0200

    robotpkg has urdf and lunch filesa

---
+++ Day6/robotWS/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/abdelrahman/AucRobotics/Day6/robotWS/src
+# Build directory: /home/abdelrahman/AucRobotics/Day6/robotWS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robotpkg")
---
+++ Day6/robotWS/build/robotpkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/abdelrahman/AucRobotics/Day6/robotWS/src/robotpkg
+# Build directory: /home/abdelrahman/AucRobotics/Day6/robotWS/build/robotpkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
